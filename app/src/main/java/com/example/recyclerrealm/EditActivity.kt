f70f2de90c2c24c5490fb19c2944278f6bc8e5b7
YChaeeun <se_same5315@naver.com>
2019-08-07 23:10:03 +0900

    feature-#2 [Realm] 6-1. 데이터 추가하기
    - maxId()로 제일 큰 id 값을 가져온 뒤, id+1 자리에 데이터 추가

--- app/src/main/java/com/example/recyclerrealm/EditActivity.kt
+++ app/src/main/java/com/example/recyclerrealm/EditActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
@@ -15,6 +17,8 @@ class EditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit)
 
         btn_submit_edit_txt.setOnClickListener {
+            insertData()
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
@@ -24,5 +28,24 @@ class EditActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         realm.close()
+    }
+
+    // 데이터 추가하기
+    private fun insertData() {
+        realm.beginTransaction()
+
+        val newItem = realm.createObject<MeasureUnit>(nextId())
+        newItem.unitNameBold = edit_txt_name_bold.text.toString()
+        newItem.unitNameSoft = edit_txt_name_bold.text.toString()
+
+        realm.commitTransaction()
+    }
+
+    private fun nextId() : Int {
+        val maxId = realm.where<MeasureUnit>().max("unitId")
+        if (maxId != null) {
+            return maxId.toInt() + 1
+        }
+        return 0
     }
 }
