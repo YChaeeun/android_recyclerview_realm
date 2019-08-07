dfef6423a6d7ad936242b296c9c225f1d55f134b
YChaeeun <se_same5315@naver.com>
2019-08-07 22:49:55 +0900

    feature-#2 [Realm] 5. Realm 인스턴스 객체 얻기 & 해제하기
    - Realm을 사용할 액티비티에서 Realm 인스턴스 객체를 얻고 해제하기

--- app/src/main/java/com/example/recyclerrealm/EditActivity.kt
+++ app/src/main/java/com/example/recyclerrealm/EditActivity.kt
@@ -3,18 +3,26 @@ package com.example.recyclerrealm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
+
+    private val realm = Realm.getDefaultInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
         btn_submit_edit_txt.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
